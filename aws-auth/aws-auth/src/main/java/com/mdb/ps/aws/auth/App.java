d2a12d445cbf1e57076e1196b9fdaf911c8cc2e8
Derek Grove <derekjgrove@gmail.com>
2023-05-26 01:04:39 -0400

    refresh token logic

--- aws-auth/aws-auth/src/main/java/com/mdb/ps/aws/auth/App.java
+++ aws-auth/aws-auth/src/main/java/com/mdb/ps/aws/auth/App.java
@@ -1,4 +1,5 @@
 package com.mdb.ps.aws.auth;
+import com.mongodb.AwsCredential;
 import com.mongodb.ConnectionString;
 import com.mongodb.MongoClientSettings;
 import com.mongodb.MongoException;
@@ -12,13 +13,67 @@ import com.mongodb.MongoCredential;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
+import java.time.Instant;
+import java.util.function.Supplier;
+
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.sts.StsClient;
+import software.amazon.awssdk.services.sts.model.AssumeRoleRequest;
+import software.amazon.awssdk.services.sts.model.AssumeRoleResponse;
+import software.amazon.awssdk.services.sts.model.Credentials;
 
 
 /**
  * Hello world!
  */
 public final class App {
-    private App() {
+    private App() {}
+
+    private static class MongoAwsCredentialSupplier implements Supplier<AwsCredential> {
+        private final Supplier<Credentials> wrappedSupplier;
+        private Credentials credentials;
+ 
+        public MongoAwsCredentialSupplier(Supplier<Credentials> wrappedSupplier) {
+            this.wrappedSupplier = wrappedSupplier;
+            credentials = wrappedSupplier.get();
+        }
+ 
+        @Override
+        public AwsCredential get() {
+            synchronized (this) {
+                // alternatively, could start a thread that keeps the credentials up to date, in order to avoid blocking 
+                if (credentials.expiration().isAfter(Instant.now().minusSeconds(60))) {
+                    credentials = wrappedSupplier.get();
+                }
+            }
+            return new AwsCredential(
+                    credentials.accessKeyId(),
+                    credentials.secretAccessKey(),
+                    credentials.sessionToken());
+        }
+    }
+ 
+    private static class CredentialsSupplier implements Supplier<Credentials> {
+        private final StsClient stsClient;
+        private final String roleArn;
+        private final String roleSessionName;
+ 
+        public CredentialsSupplier(StsClient stsClient, String roleArn, String roleSessionName) {
+            this.stsClient = stsClient;
+            this.roleArn = roleArn;
+            this.roleSessionName = roleSessionName;
+        }
+ 
+        @Override
+        public Credentials get() {
+            AssumeRoleRequest roleRequest = AssumeRoleRequest.builder()
+                    .roleArn(roleArn)
+                    .roleSessionName(roleSessionName)
+                    .build();
+ 
+            AssumeRoleResponse roleResponse = stsClient.assumeRole(roleRequest);
+            return roleResponse.credentials();
+        }
     }
 
     /**
@@ -34,6 +89,10 @@ public final class App {
         String token_raw = "<token>";
         String encoder = "UTF-8";
 
+        String region_raw = "us-east-1";
+        String roleArn = "arn:aws:iam::############:role/<role>";
+        String roleSession = "<session_name>";
+
         String username = URLEncoder.encode(username_raw, encoder);
         String password = URLEncoder.encode(password_raw, encoder);
         String token = URLEncoder.encode(token_raw, encoder);
@@ -42,11 +101,24 @@ public final class App {
             .version(ServerApiVersion.V1)
             .build();
 
+
+
+        Region region = Region.of(region_raw);
+        StsClient stsClient = StsClient.builder()
+                .region(region)
+                .build();
+
+
+
         // MongoCredential
 
         try {
-            MongoCredential credential = MongoCredential.createAwsCredential(username_raw, password_raw.toCharArray())
-                .withMechanismProperty("AWS_SESSION_TOKEN",  token_raw);
+            MongoCredential credential = MongoCredential.createAwsCredential(null, null)
+                .withMechanismProperty(MongoCredential.AWS_CREDENTIAL_PROVIDER_KEY, 
+                    new MongoAwsCredentialSupplier(
+                        new CredentialsSupplier(stsClient, roleArn, roleSession)));
+            //MongoCredential credential = MongoCredential.createAwsCredential(username_raw, password_raw.toCharArray())
+                // .withMechanismProperty("AWS_SESSION_TOKEN",  token_raw);
             ConnectionString connectionString = new ConnectionString("mongodb+srv://<ATLAS_VIP>");
 
             System.out.println("MongoCredential --> " + credential.toString());
